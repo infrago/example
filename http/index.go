258746b2a26963953c95627d8bd978dc72f74a7d
yatniel <yatniel@qq.com>
2023-03-21 15:37:59 +0800

    http method示例

--- http/index.go
+++ http/index.go
@@ -1,6 +1,7 @@
 package trigger
 
 import (
+	. "github.com/infrago/base"
 	"github.com/infrago/http"
 	"github.com/infrago/infra"
 )
@@ -10,6 +11,27 @@ func init() {
 		Uri: "/", Name: "首页", Text: "首页",
 		Action: func(ctx *http.Context) {
 			ctx.Text("hello world.")
+		},
+	})
+
+	infra.Register(".method", http.Route{
+		Uri: "/method", Name: "方法支持", Text: "方法支持",
+		Routing: http.Routing{
+			GET: http.Route{
+				Action: func(ctx *http.Context) {
+					ctx.Text("get hello world.")
+				},
+			},
+			POST: http.Route{
+				Action: func(ctx *http.Context) {
+					ctx.Text("post hello world.")
+				},
+			},
+			PUT: http.Route{
+				Action: func(ctx *http.Context) {
+					ctx.Text("put hello world.")
+				},
+			},
 		},
 	})
 }
