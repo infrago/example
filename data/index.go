364f9e97742ef67b3bea19f0a594f6a55a742bae
yatniel <yatniel@qq.com>
2023-05-23 08:51:01 +0800

    up

--- data/index.go
+++ data/index.go
@@ -9,11 +9,67 @@ import (
 
 func init() {
 
+	infra.Register("user.Login", infra.Method{
+		Name: "用户登录", Text: "用户登录",
+		Args: Vars{
+			"account":  Var{Type: "string", Required: true},
+			"password": Var{Type: "password", Required: true},
+		},
+		Action: func(ctx *http.Context) (Map, Res) {
+			db := data.Base()
+			defer db.Close()
+
+			account := ctx.Args["account"].(string)
+			password := ctx.Args["password"].(string)
+
+			user := db.Table("user").First(Map{
+				"account": account,
+			})
+			if user == nil {
+				return nil, LoginNotExist
+			}
+
+			if user["password"] != password {
+				return nil, LoginPassword
+			}
+
+			return user, LoginOK
+		},
+	})
+
 	infra.Register("test", data.Table{
 		Name: "测试", Text: "测试",
 		Fields: Vars{
 			"id":  Var{Type: "int", Required: false},
 			"msg": Var{Type: "string", Required: true},
+		},
+	})
+
+	infra.Register(".login", http.Router{
+		Uri: "/login", Name: "login", Text: "login",
+		Routing: http.Routing{
+			GET: http.Router{
+				Action: func(ctx *http.Context) {
+					ctx.View("login")
+				},
+			},
+			POST: http.Router{
+				Args: Vars{
+					"account":  Var{Type: "string", Required: true},
+					"password": Var{Type: "password", Required: true},
+				},
+				Action: func(ctx *http.Context) {
+					user := ctx.Invoke("user.Login", ctx.Args)
+					if res := ctx.Result(); res.Fail() {
+						ctx.Answer(res)
+					} else {
+						//成功
+						ctx.Answer(res, Map{
+							"user": user,
+						})
+					}
+				},
+			},
 		},
 	})
 
